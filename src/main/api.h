f6faebd0aa7e1674b4e58e2ddcd7d8ad4755a3da
mudlamp <searchmarlin@gmail.com>
2018-09-24 22:52:38 -0700

    Added api handlers and include klib

--- src/main/api.h
+++ src/main/api.h
@@ -1,9 +1,30 @@
 #ifndef _API_H
 #define _API_H
 #include <h2o.h>
+#include "khash.h"
+
+#define M_APP_ID "x-marlin-application-id"
+#define M_APP_ID_LEN 23
+#define M_API_KEY "x-marlin-rest-api-key"
+#define M_API_KEY_LEN 21
+
+struct url_cbdata {
+    char *(*cb)(h2o_req_t *req, void *);
+    void *data;
+};
+
+KHASH_MAP_INIT_STR(URL_CBDATA, struct url_cbdata *)
 
 void init_api(void);
 void *run_loop(void *thread_index);
+
+
+struct url_cbdata *url_cbdata_new(char *(*cb)(h2o_req_t *, void *), void *data);
+void register_api_callback(const char *appid, const char *apikey, const char *method, 
+        const char *url, struct url_cbdata *cbdata);
+void deregister_api_callback(const char *appid, const char *apikey, const char *method, 
+        const char *url);
+char *api_forbidden(h2o_req_t *req, void *data);
 
 #endif
 
