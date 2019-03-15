a132ad8180e85bbdc784820d2650016cce6a7449
marlindev <searchmarlin@gmail.com>
2019-03-15 03:01:32 -0700

    Done with filters

--- src/main/filter_apply.c
+++ src/main/filter_apply.c
@@ -93,7 +93,110 @@ static void or_filter(struct sindex *si, struct filter *f, MDB_txn *txn, struct 
         }
     }
     f->fr_bmap = oper_or(bso);
+    if (bmap_cardinality(f->fr_bmap) == 0) {
+        bmap_free(f->fr_bmap);
+        f->fr_bmap = NULL;
+    }
     oper_free(bso);
+}
+
+static void and_filter(struct sindex *si, struct filter *f, MDB_txn *txn, struct bmap *docs) {
+    struct oper *bso = oper_new();
+    for (int i = 0; i < kv_size(f->children); i++) {
+        struct filter *c = kv_A(f->children, i);
+        if (c->fr_bmap) {
+            oper_add(bso, c->fr_bmap);
+        }
+    }
+    f->fr_bmap = oper_and(bso);
+    if (bmap_cardinality(f->fr_bmap) == 0) {
+        bmap_free(f->fr_bmap);
+        f->fr_bmap = NULL;
+    }
+    oper_free(bso);
+}
+
+/* Not in filter */
+static void nin_filter(struct sindex *si, struct filter *f, MDB_txn *txn, struct bmap *docs) {
+    // Do an in filter first and invert the result
+    or_filter(si, f, txn, docs);
+    // If no results are found, set bitmap to all valid objids
+    if (!f->fr_bmap) {
+        f->fr_bmap = bmap_duplicate(docs);
+    } else {
+        struct bmap *b = bmap_invert(f->fr_bmap, docs);
+        bmap_free(f->fr_bmap);
+        f->fr_bmap = b;
+    }
+}
+
+static inline bool check_double_oper(FILTER_TYPE f, double a, double b) {
+    switch(f) {
+        case F_LT:
+            return a<b;
+            break;
+        case F_LTE:
+            return a<=b;
+            break;
+        case F_GT:
+            return a>b;
+            break;
+        case F_GTE:
+            return a>=b;
+            break;
+        default:
+            return false;
+    }
+}
+
+static void num_filter(struct sindex *si, struct filter *f, MDB_txn *txn, struct bmap *docs) {
+    if (f->field_type != F_NUMBER) return;
+
+    bool gt = (f->type == F_GT || f->type == F_GTE);
+    f->fr_bmap = bmap_new();
+    double val = f->numval;
+    MDB_val key, data;
+    key.mv_size = sizeof(double);
+    key.mv_data = &val;
+    MDB_cursor *cursor;
+    mdb_cursor_open(txn, si->num_dbi[f->s->i_priority], &cursor);
+    MDB_cursor_op op = gt ? MDB_FIRST : MDB_LAST;
+    MDB_cursor_op op2 = gt ? MDB_NEXT : MDB_PREV;
+    while(mdb_cursor_get(cursor, &key, &data, op) == 0) {
+        op = op2;
+        double *kd = key.mv_data;
+        uint64_t *dd = data.mv_data;
+        if (check_double_oper(f->type, *kd, f->numval)) {
+            bmap_add(f->fr_bmap, (uint32_t) *dd);
+        } else break;
+    }
+    mdb_cursor_close(cursor);
+}
+
+static void num_cmp_filter(struct sindex *si, struct filter *f, MDB_txn *txn, struct bmap *docs) {
+    if (f->field_type != F_NUMBER) return;
+
+    f->fr_bmap = bmap_new();
+    double val = f->numval2;
+    MDB_val key, data;
+    key.mv_size = sizeof(double);
+    key.mv_data = &val;
+    MDB_cursor *cursor;
+    mdb_cursor_open(txn, si->num_dbi[f->s->i_priority], &cursor);
+    MDB_cursor_op op = MDB_SET_RANGE;
+
+    while(mdb_cursor_get(cursor, &key, &data, op) == 0) {
+        op = MDB_NEXT;
+        double *kd = key.mv_data;
+        uint64_t *dd = data.mv_data;
+        if (check_double_oper(f->numcmp2, *kd, f->numval2)) {
+            if (check_double_oper(f->numcmp1, *kd, f->numval)) {
+                bmap_add(f->fr_bmap, (uint32_t) *dd);
+            } else break;
+        }
+    }
+
+    mdb_cursor_close(cursor);
 }
 
 
@@ -119,5 +222,12 @@ void init_filter_callbacks(void) {
     filter_callback[F_EQ] = eq_filter;
     filter_callback[F_NE] = ne_filter;
     filter_callback[F_OR] = or_filter;
+    filter_callback[F_NIN] = nin_filter;
+    filter_callback[F_AND] = and_filter;
+    filter_callback[F_GT] = num_filter;
+    filter_callback[F_GTE] = num_filter;
+    filter_callback[F_LT] = num_filter;
+    filter_callback[F_LTE] = num_filter;
+    filter_callback[F_NUMCMP] = num_cmp_filter;
     filter_callback[F_ERROR] = error_filter;
 }
